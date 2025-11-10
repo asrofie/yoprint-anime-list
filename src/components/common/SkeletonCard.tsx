75d68a487ee70d971f44731e076cfe3adbf6168b
asrofie-work <upik91@gmail.com>
2025-11-10 07:59:41 +0700

    adding logo

--- src/components/common/SkeletonCard.tsx
+++ src/components/common/SkeletonCard.tsx
@@ -1,12 +1,15 @@
 import { Card, CardContent, Skeleton, Stack } from '@mui/material'
 export default function SkeletonCard() {
   return (
-    <Card>
+    <Card style={{
+           justifyContent: 'flex-end' 
+          }}>
       <CardContent>
         <Stack spacing={1}>
-          <Skeleton variant="rectangular" height={180} />
+          <Skeleton variant="rectangular" height={220} />
           <Skeleton variant="text" />
           <Skeleton variant="text" width="60%" />
+          <Skeleton variant="text" width="40%" />
         </Stack>
       </CardContent>
     </Card>
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,6 +1,7 @@
 import { AppBar, Toolbar, Typography, Container, Stack, ToggleButtonGroup, ToggleButton } from '@mui/material'
 import { useTranslation } from 'react-i18next'
 import { Link } from 'react-router-dom'
+import logo from '@/assets/logo.png'
 
 export default function Header() {
   const { t, i18n } = useTranslation()
@@ -13,9 +14,13 @@ export default function Header() {
     <AppBar position="sticky" color="default" elevation={0} sx={{ borderBottom: 1, borderColor: 'divider' }}>
       <Toolbar>
         <Container sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
-          <Typography variant="h6" component={Link} to="/" sx={{ textDecoration: 'none' }}>
-            {t('anime-finder')}
-          </Typography>
+          
+          <Stack direction="row" spacing={1} component={Link} to="/" sx={{ textDecoration: 'none', alignItems: 'center' }}>
+            <img src={logo} alt="Logo" style={{ height: 60, width: 'auto' }} />
+            <Typography variant="h6">
+              {t('anime-finder')}
+            </Typography>
+          </Stack>
           <Stack direction="row" alignItems="center" spacing={2}>
             <ToggleButtonGroup size="small" exclusive value={lang} onChange={handleLang} aria-label="language">
               <ToggleButton value="en" aria-label="English">EN</ToggleButton>
